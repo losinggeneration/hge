bdb69c3c45319f4c42a122b63fe55aabc3e2c238
Harley Laue <losinggeneration@gmail.com>
2012-08-02 01:59:21 -0500

    Tutorial02 now working
    * Also ran go fmt over the source

--- tutorial02/main.go
+++ tutorial02/main.go
@@ -4,13 +4,13 @@
 ** hge.relishgames.com
 **
 ** hge_tut02 - Using input, sound and rendering
-*/
+ */
 
 package main
 
 import (
-	"hge"
 	"fmt"
+	"hge"
 )
 
 var h *hge.HGE
@@ -20,14 +20,14 @@ var quad hge.Quad
 var snd hge.Effect
 
 var (
-	x float32
-	y float32
+	x  float32
+	y  float32
 	dx float32
 	dy float32
 )
 
 const (
-	speed = 90
+	speed    = 90
 	friction = 0.98
 )
 
@@ -39,35 +39,65 @@ func setup() {
 }
 
 func boom() {
-	pan := int((x-400)/4)
-	pitch := (dx*dx+dy*dy)*0.0005+0.2
-	h.Effect_PlayEx(snd,100,pan,pitch)
+	pan := int((x - 400) / 4)
+	pitch := (dx*dx+dy*dy)*0.0005 + 0.2
+	h.Effect_PlayEx(snd, 100, pan, pitch)
 }
 
 func FrameFunc() int {
-	dt := h.Timer_GetDelta();
+	dt := h.Timer_GetDelta()
 
-	if h.Input_GetKeyState(hge.K_ESCAPE) { return 1 }
-	if h.Input_GetKeyState(hge.K_LEFT) { dx-=speed*dt }
-	if h.Input_GetKeyState(hge.K_RIGHT) { dx+=speed*dt }
-	if h.Input_GetKeyState(hge.K_UP) { dy-=speed*dt }
-	if h.Input_GetKeyState(hge.K_DOWN) { dy+=speed*dt }
+	if h.Input_GetKeyState(hge.K_ESCAPE) {
+		return 1
+	}
+	if h.Input_GetKeyState(hge.K_LEFT) {
+		dx -= speed * dt
+	}
+	if h.Input_GetKeyState(hge.K_RIGHT) {
+		dx += speed * dt
+	}
+	if h.Input_GetKeyState(hge.K_UP) {
+		dy -= speed * dt
+	}
+	if h.Input_GetKeyState(hge.K_DOWN) {
+		dy += speed * dt
+	}
 
 	// Do some movement calculations and collision detection
-	dx*=friction
-	dy*=friction
-	x+=dx
-	y+=dy
-	if x>784  {x=784-(x-784);dx=-dx;boom();}
-	if x<16  {x=16+16-x;dx=-dx;boom();}
-	if y>584  {y=584-(y-584);dy=-dy;boom();}
-	if y<16  {y=16+16-y;dy=-dy;boom();}
+	dx *= friction
+	dy *= friction
+	x += dx
+	y += dy
+	if x > 784 {
+		x = 784 - (x - 784)
+		dx = -dx
+		boom()
+	}
+	if x < 16 {
+		x = 16 + 16 - x
+		dx = -dx
+		boom()
+	}
+	if y > 584 {
+		y = 584 - (y - 584)
+		dy = -dy
+		boom()
+	}
+	if y < 16 {
+		y = 16 + 16 - y
+		dy = -dy
+		boom()
+	}
 
 	// Set up quad's screen coordinates
-//	quad.v[0].x=x-16; quad.v[0].y=y-16;
-//	quad.v[1].x=x+16; quad.v[1].y=y-16;
-//	quad.v[2].x=x+16; quad.v[2].y=y+16;
-//	quad.v[3].x=x-16; quad.v[3].y=y+16;
+	quad.V[0].X = x - 16
+	quad.V[0].Y = y - 16
+	quad.V[1].X = x + 16
+	quad.V[1].Y = y - 16
+	quad.V[2].X = x + 16
+	quad.V[2].Y = y + 16
+	quad.V[3].X = x - 16
+	quad.V[3].Y = y + 16
 
 	// Continue execution
 	return 0
@@ -80,17 +110,17 @@ func RenderFunc() int {
 	// Begin rendering quads.
 	// This function must be called
 	// before any actual rendering.
-	h.Gfx_BeginScene();
+	h.Gfx_BeginScene()
 
 	// Clear screen with black color
-	h.Gfx_Clear(0);
+	h.Gfx_Clear(0)
 
 	// Render quads here. This time just
 	// one of them will serve our needs.
-	h.Gfx_RenderQuad(quad);
+	h.Gfx_RenderQuad(&quad)
 
 	// End rendering and update the screen
-	h.Gfx_EndScene();
+	h.Gfx_EndScene()
 
 	// RenderFunc should always return false
 	return 0
@@ -99,56 +129,60 @@ func RenderFunc() int {
 func main() {
 	setup()
 	// Get HGE interface
-	h = hge.Create(hge.VERSION);
+	h = hge.Create(hge.VERSION)
 
 	// Set up log file, frame function, render function and window title
-	h.System_SetState(hge.LOGFILE, "hge_tut02.log");
-	h.System_SetState(hge.FRAMEFUNC, FrameFunc);
-	h.System_SetState(hge.RENDERFUNC, RenderFunc);
-	h.System_SetState(hge.TITLE, "HGE Tutorial 02 - Using input, sound and rendering");
+	h.System_SetState(hge.LOGFILE, "hge_tut02.log")
+	h.System_SetState(hge.FRAMEFUNC, FrameFunc)
+	h.System_SetState(hge.RENDERFUNC, RenderFunc)
+	h.System_SetState(hge.TITLE, "HGE Tutorial 02 - Using input, sound and rendering")
 
 	// Set up video mode
-	h.System_SetState(hge.WINDOWED, true);
-	h.System_SetState(hge.SCREENWIDTH, 800);
-	h.System_SetState(hge.SCREENHEIGHT, 600);
-	h.System_SetState(hge.SCREENBPP, 32);
+	h.System_SetState(hge.WINDOWED, true)
+	h.System_SetState(hge.SCREENWIDTH, 800)
+	h.System_SetState(hge.SCREENHEIGHT, 600)
+	h.System_SetState(hge.SCREENBPP, 32)
 
 	if h.System_Initiate() {
 		// Load sound and texture
-		snd=h.Effect_Load("menu.ogg");
-//		quad.tex=h.Texture_Load("particles.png");
-//		if(!snd || !quad.tex) {
-//			// If one of the data files is not found, display
-//			// an error message and shutdown.
-//			fmt.Println("Error: Can't load menu.wav or particles.png");
-//			h.System_Shutdown();
-//			h.Release();
-//			return 0;
-//		}
+		snd = h.Effect_Load("menu.ogg")
+		quad.Tex = h.Texture_Load("particles.png")
+		if snd == 0 || quad.Tex == 0 {
+			// If one of the data files is not found, display
+			// an error message and shutdown.
+			fmt.Println("Error: Can't load menu.wav or particles.png")
+			h.System_Shutdown()
+			h.Release()
+			return
+		}
 
 		// Set up quad which we will use for rendering sprite
-//		quad.blend=hge.BLEND_ALPHAADD | hge.BLEND_COLORMUL | hge.BLEND_ZWRITE;
+		quad.Blend = hge.BLEND_ALPHAADD | hge.BLEND_COLORMUL | hge.BLEND_ZWRITE
 
-//		for i=0;i<4;i++ {
-//			// Set up z-coordinate of vertices
-//			quad.v[i].z=0.5;
-//			// Set up color. The format of DWORD col is 0xAARRGGBB
-//			quad.v[i].col=0xFFFFA000;
-//		}
+		for i := 0; i < 4; i++ {
+			// Set up z-coordinate of vertices
+			quad.V[i].Z = 0.5
+			// Set up color. The format of DWORD col is 0xAARRGGBB
+			quad.V[i].Col = 0xFFFFA000
+		}
 
 		// Set up quad's texture coordinates.
 		// 0,0 means top left corner and 1,1 -
 		// bottom right corner of the texture.
-//		quad.v[0].tx=96.0/128.0; quad.v[0].ty=64.0/128.0;
-//		quad.v[1].tx=128.0/128.0; quad.v[1].ty=64.0/128.0;
-//		quad.v[2].tx=128.0/128.0; quad.v[2].ty=96.0/128.0;
-//		quad.v[3].tx=96.0/128.0; quad.v[3].ty=96.0/128.0;
+		quad.V[0].TX = 96.0 / 128.0
+		quad.V[0].TY = 64.0 / 128.0
+		quad.V[1].TX = 128.0 / 128.0
+		quad.V[1].TY = 64.0 / 128.0
+		quad.V[2].TX = 128.0 / 128.0
+		quad.V[2].TY = 96.0 / 128.0
+		quad.V[3].TX = 96.0 / 128.0
+		quad.V[3].TY = 96.0 / 128.0
 
 		// Let's rock now!
-		h.System_Start();
+		h.System_Start()
 
 		// Free loaded texture and sound
-//		h.Texture_Free(quad.tex)
+		h.Texture_Free(quad.Tex)
 		h.Effect_Free(snd)
 	} else {
 		fmt.Println("Error: %s\n", h.System_GetErrorMessage())
@@ -158,4 +192,3 @@ func main() {
 	h.System_Shutdown()
 	h.Release()
 }
-
