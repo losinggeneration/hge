c4cadd7b234e997197d98fe395a5953e2d68dd92
Harley Laue <losinggeneration@gmail.com>
2012-08-20 03:40:21 -0500

    Update tutorial03
    * This updates tutorial03 to do some more interesting things, like run
      the entire program as a function (to try and clean up memory from
      hge.New()). This also shows an alternative approach to using global
      functions for the render/frame functions (and using closures within
      them.)

--- tutorials/tutorial03/main.go
+++ tutorials/tutorial03/main.go
@@ -10,144 +10,150 @@ import (
 	. "github.com/losinggeneration/hge-go/hge/input"
 	. "github.com/losinggeneration/hge-go/hge/sound"
 	. "github.com/losinggeneration/hge-go/hge/timer"
+	"runtime"
+	"time"
 )
 
-var (
-	spr, spt sprite.Sprite
-	fnt      *font.Font
-	par      *particle.ParticleSystem
-
-	tex Texture
-	snd Effect
-
-	x  = 100.0
-	y  = 100.0
-	dx = 0.0
-	dy = 0.0
-)
-
-const (
-	speed    = 90.0
-	friction = 0.98
-)
-
-func boom() {
-	pan := int((x - 400) / 4)
-	pitch := (dx*dx+dy*dy)*0.0005 + 0.2
-	snd.PlayEx(100, pan, pitch)
-}
-
-func FrameFunc() int {
-	dt := float64(Delta())
-
-	// Process keys
-	if NewKey(K_ESCAPE).State() {
-		return 1
-	}
-	if NewKey(K_LEFT).State() {
-		dx -= speed * dt
-	}
-	if NewKey(K_RIGHT).State() {
-		dx += speed * dt
-	}
-	if NewKey(K_UP).State() {
-		dy -= speed * dt
-	}
-	if NewKey(K_DOWN).State() {
-		dy += speed * dt
-	}
-
-	// Do some movement calculations and collision detection
-	dx *= friction
-	dy *= friction
-	x += dx
-	y += dy
-	if x > 784 {
-		x = 784 - (x - 784)
-		dx = -dx
-		boom()
-	}
-	if x < 16 {
-		x = 16 + 16 - x
-		dx = -dx
-		boom()
-	}
-	if y > 584 {
-		y = 584 - (y - 584)
-		dy = -dy
-		boom()
-	}
-	if y < 16 {
-		y = 16 + 16 - y
-		dy = -dy
-		boom()
-	}
-
-	// Update particle system
-	par.Info.Emission = (int)(dx*dx+dy*dy) * 2
-	par.MoveTo(x, y)
-	par.Update(dt)
-
-	return 0
-}
-
-func RenderFunc() int {
-	BeginScene()
-	Clear(0)
-	// currently broken
-	par.Render()
-	spr.Render(x, y)
-	fnt.Printf(5, 5, font.TEXT_LEFT, "dt:%.3f\nFPS:%d (constant)", Delta(), GetFPS())
-	EndScene()
-
-	return 0
-}
-
+// An example of using closures
 func main() {
-	defer Free()
+	func() {
+		const (
+			speed    = 90.0
+			friction = 0.98
+		)
 
-	SetState(LOGFILE, "tutorial03.log")
-	SetState(FRAMEFUNC, FrameFunc)
-	SetState(RENDERFUNC, RenderFunc)
-	SetState(TITLE, "HGE Tutorial 03 - Using helper classes")
-	SetState(FPS, 100)
-	SetState(WINDOWED, true)
-	SetState(SCREENWIDTH, 800)
-	SetState(SCREENHEIGHT, 600)
-	SetState(SCREENBPP, 32)
+		var (
+			spr sprite.Sprite
+			fnt *font.Font
+			par *particle.ParticleSystem
 
-	if err := Initiate(); err == nil {
-		defer Shutdown()
-		snd = NewEffect("menu.ogg")
-		tex = LoadTexture("particles.png")
-		if snd == 0 || tex == 0 {
-			fmt.Printf("Error: Can't load one of the following files:\nmenu.ogg, particles.png, font1.fnt, font1.png, trail.psi\n")
-			return
+			snd Effect
+
+			x  = 100.0
+			y  = 100.0
+			dx = 0.0
+			dy = 0.0
+		)
+
+		hge := New()
+
+		hge.SetState(LOGFILE, "tutorial03.log")
+		hge.SetState(TITLE, "HGE Tutorial 03 - Using helper classes")
+		hge.SetState(FPS, 100)
+		hge.SetState(WINDOWED, true)
+		hge.SetState(SCREENWIDTH, 800)
+		hge.SetState(SCREENHEIGHT, 600)
+		hge.SetState(SCREENBPP, 32)
+		hge.SetState(FRAMEFUNC, func() int {
+			dt := float64(Delta())
+
+			boom := func() {
+				pan := int((x - 400) / 4)
+				pitch := (dx*dx+dy*dy)*0.0005 + 0.2
+				snd.PlayEx(100, pan, pitch)
+			}
+
+			// Process keys
+			if NewKey(K_ESCAPE).State() {
+				return 1
+			}
+			if NewKey(K_LEFT).State() {
+				dx -= speed * dt
+			}
+			if NewKey(K_RIGHT).State() {
+				dx += speed * dt
+			}
+			if NewKey(K_UP).State() {
+				dy -= speed * dt
+			}
+			if NewKey(K_DOWN).State() {
+				dy += speed * dt
+			}
+
+			// Do some movement calculations and collision detection
+			dx *= friction
+			dy *= friction
+			x += dx
+			y += dy
+			if x > 784 {
+				x = 784 - (x - 784)
+				dx = -dx
+				boom()
+			}
+			if x < 16 {
+				x = 16 + 16 - x
+				dx = -dx
+				boom()
+			}
+			if y > 584 {
+				y = 584 - (y - 584)
+				dy = -dy
+				boom()
+			}
+			if y < 16 {
+				y = 16 + 16 - y
+				dy = -dy
+				boom()
+			}
+
+			// Update particle system
+			par.Info.Emission = (int)(dx*dx+dy*dy) * 2
+			par.MoveTo(x, y)
+			par.Update(dt)
+
+			return 0
+		})
+
+		hge.SetState(RENDERFUNC, func() int {
+			BeginScene()
+			Clear(0)
+			par.Render()
+			spr.Render(x, y)
+			fnt.Printf(5, 5, font.TEXT_LEFT, "dt:%.3f\nFPS:%d (constant)", Delta(), GetFPS())
+			EndScene()
+
+			return 0
+		})
+
+		if err := hge.Initiate(); err == nil {
+			defer hge.Shutdown()
+			snd = NewEffect("menu.ogg")
+			tex := LoadTexture("particles.png")
+			if snd == 0 || tex == nil {
+				fmt.Printf("Error: Can't load one of the following files:\nmenu.ogg, particles.png, font1.fnt, font1.png, trail.psi\n")
+				return
+			}
+
+			defer snd.Free()
+			// 			defer tex.Free()
+
+			spr = sprite.NewSprite(tex, 96, 64, 32, 32)
+			spr.SetColor(0xFFFFA000)
+			spr.SetHotSpot(16, 16)
+
+			if fnt = font.NewFont("font1.fnt"); fnt == nil {
+				fmt.Println("Error loading font1.fnt")
+				return
+			}
+
+			spt := sprite.NewSprite(tex, 32, 32, 32, 32)
+			spt.SetBlendMode(BLEND_COLORMUL | BLEND_ALPHAADD | BLEND_NOZWRITE)
+			spt.SetHotSpot(16, 16)
+
+			par = particle.NewParticleSystem("trail.psi", spt)
+			if par == nil {
+				fmt.Println("Error loading trail.psi")
+				return
+			}
+			par.Fire()
+
+			hge.Start()
 		}
+	}()
 
-		defer snd.Free()
-		defer tex.Free()
-
-		spr = sprite.NewSprite(tex, 96, 64, 32, 32)
-		spr.SetColor(0xFFFFA000)
-		spr.SetHotSpot(16, 16)
-
-		if fnt = font.NewFont("font1.fnt"); fnt == nil {
-			fmt.Println("Error loading font1.fnt")
-			return
-		}
-
-		spt = sprite.NewSprite(tex, 32, 32, 32, 32)
-		spt.SetBlendMode(BLEND_COLORMUL | BLEND_ALPHAADD | BLEND_NOZWRITE)
-		spt.SetHotSpot(16, 16)
-
-		par = particle.NewParticleSystem("trail.psi", spt)
-		if par == nil {
-			fmt.Println("Error loading trail.psi")
-			return
-		}
-		par.Fire()
-
-		Start()
+	for i := 0; i < 10; i++ {
+		fmt.Println("Sleeping")
+		time.Sleep(1 * time.Second)
+		runtime.GC()
 	}
 }
