a83bc7f16387c817c69e5e489fa00ce8d9e1a7de
Harley Laue <losinggeneration@gmail.com>
2012-11-30 13:38:22 -0600

    Binding: Allow func() bool as a FuncState

--- hge/hge.go
+++ hge/hge.go
@@ -262,6 +262,18 @@ func (h *HGE) SetState(a ...interface{}) {
 				h.setStateFunc(a[0].(FuncState), a[1].(func() int))
 				return
 			}
+		case func() bool:
+			h.setStateFunc(a[0].(FuncState), func() int {
+				if a[1].(func() bool)() {
+					return 1
+				}
+
+				return 0
+			})
+			return
+		default:
+			h.setStateFunc(a[0].(FuncState), nil)
+			return
 
 		case HwndState:
 			if hs, ok := a[1].(*Hwnd); ok {
